dd0fef5b947a26a785ba68de35b03412688a3de2
Anisiia Yalyzhko <anisiya.sumy.ua@gmail.com>
2026-05-07 00:01:22 +0300

    Add unit tests for validation and exceptions

--- src/test/java/ua/edu/sumdu/j2se/pr4/BookTest.java
+++ src/test/java/ua/edu/sumdu/j2se/pr4/BookTest.java
@@ -1,4 +1,26 @@
 package ua.edu.sumdu.j2se.pr4;
 
-public class BookTest {
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookTest {
+
+    @Test
+    void shouldThrowExceptionWhenPriceIsNegative() {
+        // Створюємо книгу з нормальною ціною
+        Book book = new Book("Назва", "Автор", 2024, 100.0);
+
+        // Перевіряємо, що сетер викине помилку, якщо спробувати поставити -50
+        assertThrows(IllegalArgumentException.class, () -> {
+            book.setPrice(-50.0);
+        });
+    }
+
+    @Test
+    void shouldThrowExceptionWhenConstructorDataIsInvalid() {
+        // Перевіряємо, що конструктор викине помилку, якщо назва порожня
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Book("", "Автор", 2024, 100.0);
+        });
+    }
 }
