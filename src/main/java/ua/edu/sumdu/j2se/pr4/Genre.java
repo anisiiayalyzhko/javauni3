c9459e2639ca443bbd6cad490e30c3df54575367
Anisiia Yalyzhko <anisiya.sumy.ua@gmail.com>
2026-05-07 00:59:03 +0300

    Add Genre enum for fixed book categories

---
+++ src/main/java/ua/edu/sumdu/j2se/pr4/Genre.java
@@ -0,0 +1,9 @@
+package ua.edu.sumdu.j2se.pr4;
+
+public enum Genre {
+    FICTION,    // Художня
+    NON_FICTION,// Наукова
+    FANTASY,    // Фентезі
+    HISTORY,    // Історична
+    CLASSIC     // Класика
+}
